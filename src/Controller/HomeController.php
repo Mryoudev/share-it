4ccdd6526a84eec0a3e0110b0a31f7d00c1b82ad
JonesDev <youness.agnaou@outlook.com>
2020-11-24 15:17:16 +0100

    Please enter the commit message for your changes. Lines starting

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,17 +4,27 @@ namespace App\Controller;
 
 use Doctrine\DBAL\Connection;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController extends AbstractController
 {
-    public function homepage(ResponseInterface $response)
+    public function homepage(ResponseInterface $response, ServerRequestInterface $request)
     {
-        return $this->template($response, 'home.html.twig');
+
+        $request->getUploadedFiles();
+
+    $files = $request->getUploadedFiles();
+    var_dump($files['file']);
+
+        return $this->template($response,'home.html.twig');
+
     }
 
     public function download(ResponseInterface $response, int $id)
-    {
-        $response->getBody()->write(sprintf('Identifiant: %d', $id));
-        return $response;
-    }
+{
+    $response->getBody()->write(sprintf('Identifiant: %d', $id));
+    return $response;
 }
+      
+    }
